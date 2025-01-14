bfe360e19e50f9e7f4566de25ae8a29978a4bf2b
Yeji <mnbvcxzyj@gmail.com>
2025-01-14 15:32:55 +0900

    feat: tailwind setting

---
+++ src/frontend/apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import './globals.css'
+import '@repo/ui/styles.css'
+import {Counter, Header} from "@repo/ui";
+
+function App(){
+    return <div className='text-red-800'>
+       <Counter/>
+        <Header title={"안냐세요"} />
+    </div>
+}
+
+export default App
