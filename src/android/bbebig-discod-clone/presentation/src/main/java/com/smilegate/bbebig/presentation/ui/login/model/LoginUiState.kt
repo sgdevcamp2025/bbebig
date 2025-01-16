8788540285feaf7bdc2d1a59d9b98dea9300504f
seojeongwoo <qw4643987@naver.com>
2025-01-16 18:19:08 +0900

    feat: 로그인 mvi 모델 구성

---
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/login/model/LoginUiState.kt
@@ -0,0 +1,20 @@
+package com.smilegate.bbebig.presentation.ui.login.model
+
+import com.smilegate.bbebig.presentation.base.UiState
+import com.smilegate.bbebig.presentation.model.UserInfo
+
+data class LoginUiState(
+    val userInfo: UserInfo,
+) : UiState {
+    companion object {
+        fun init(): LoginUiState = LoginUiState(
+            userInfo = UserInfo(
+                id = 0,
+                password = "",
+                name = "",
+                email = "",
+                phone = "",
+            ),
+        )
+    }
+}
