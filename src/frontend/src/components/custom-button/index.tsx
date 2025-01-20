ece4992795107f3acf96999f75c8c4e32eef02ce
Zero-1016 <iolpd42@gmail.com>
2025-01-20 18:14:05 +0900

    feat: 임시 폴더구조를 추가했습니다.

--- src/frontend/src/components/custom-button/index.tsx
+++ src/frontend/src/components/custom-button/index.tsx
@@ -1,9 +1,20 @@
-import { buttonVariants, CustomButtonVariants } from './variants'
+import { cva } from 'class-variance-authority'
+
+import { CustomButtonVariants } from './variants'
 
 interface Props {
   children: React.ReactNode
   variant: keyof typeof CustomButtonVariants
 }
+
+const buttonVariants = cva('', {
+  variants: {
+    variant: {
+      [CustomButtonVariants.primary]: 'bg-red-200',
+      [CustomButtonVariants.secondary]: 'bg-blue-200'
+    }
+  }
+})
 
 function CustomButton({ children, variant }: Props) {
   return <button className={buttonVariants({ variant })}>{children}</button>
---
+++ src/frontend/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import CustomButton from '@/components/custom-button'
+import { CustomButtonVariants } from '@/components/custom-button/variants'
+
+function App() {
+  return (
+    <>
+      <CustomButton variant={CustomButtonVariants.primary}>Click ME!</CustomButton>
+    </>
+  )
+}
+
+export default App
