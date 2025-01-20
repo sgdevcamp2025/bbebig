ab1cdd4986a8ca2bbcfd9254c91441a79480dc69
Zero-1016 <iolpd42@gmail.com>
2025-01-20 17:35:30 +0900

    feat: tailwind util 함수를 추가합니다.

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
