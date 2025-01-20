08f21456703426db329516b14dec5a7ae9b5fff6
soeun2537 <soeun2537@gmail.com>
2025-01-20 20:47:25 +0900

    feat: 시그널링 서버 예외 처리

--- src/backend/signaling-server/src/main/java/com/bbebig/signalingserver/global/response/code/error/ErrorStatus.java
+++ src/backend/signaling-server/src/main/java/com/bbebig/signalingserver/global/response/code/error/ErrorStatus.java
@@ -15,6 +15,15 @@ public enum ErrorStatus implements BaseErrorCode {
     _LOGIN_FAILURE(HttpStatus.NOT_FOUND, "404", "요청 리소스를 찾을 수 없습니다."),
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500", "서버 에러, 관리자에게 문의 바랍니다."),
     _SERVICE_UNAVAILABLE_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "503", "서버가 일시적으로 사용중지 되었습니다."),
+
+    // 다이렉트 스트리밍
+    DIRECT_STREAM_RECEIVER_ID_MISSING(HttpStatus.BAD_REQUEST, "400", "수신자 ID(receiverID)가 누락되었습니다."),
+
+    // 그룹 스트리밍
+    GROUP_STREAM_INVALID_SIGNAL(HttpStatus.BAD_REQUEST, "400", "잘못된 시그널링 요청입니다."),
+    GROUP_STREAM_CHANNEL_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "채널 ID를 찾을 수 없습니다."),
+    GROUP_STREAM_SESSION_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "세션 ID에 해당하는 사용자를 찾을 수 없습니다."),
+    GROUP_STREAM_ENDPOINT_NOT_FOUND(HttpStatus.NOT_FOUND, "404", "해당 세션에 대한 WebRtcEndpoint를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
@@ -25,17 +34,17 @@ public enum ErrorStatus implements BaseErrorCode {
     @Override
     public ErrorReasonDTO getReason() {
         return ErrorReasonDTO.builder()
-            .message(message)
-            .code(code)
-            .build();
+                .message(message)
+                .code(code)
+                .build();
     }
 
     @Override
     public ErrorReasonDTO getReasonHttpStatus() {
         return ErrorReasonDTO.builder()
-            .message(message)
-            .code(code)
-            .httpStatus(httpStatus)
-            .build();
+                .message(message)
+                .code(code)
+                .httpStatus(httpStatus)
+                .build();
     }
 }
