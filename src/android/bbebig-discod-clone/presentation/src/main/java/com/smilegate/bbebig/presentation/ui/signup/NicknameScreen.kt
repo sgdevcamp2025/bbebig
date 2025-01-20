e646682da0a51d7935f9ac75d2533e45011e07ba
seojeongwoo <qw4643987@naver.com>
2025-01-20 16:08:12 +0900

    feat: 닉네임 입력 화면 구현

---
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/signup/NicknameScreen.kt
@@ -0,0 +1,70 @@
+package com.smilegate.bbebig.presentation.ui.signup
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.smilegate.bbebig.presentation.component.DiscordInputContainer
+import com.smilegate.bbebig.presentation.component.DiscordRoundButton
+import com.smilegate.bbebig.presentation.component.DiscordTopBar
+import com.smilegate.bbebig.presentation.theme.Blue70
+import com.smilegate.bbebig.presentation.theme.White
+import com.smilegate.devcamp.presentation.R
+
+@Composable
+fun NicknameScreen(
+    modifier: Modifier,
+    onBackClick: () -> Unit,
+    onClickConfirm: (String) -> Unit,
+) {
+    val nicknameState = rememberTextFieldState()
+
+    Column(
+        modifier = modifier.padding(horizontal = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        DiscordTopBar(
+            modifier = Modifier.fillMaxWidth(),
+            onBackClick = onBackClick,
+            backButtonColor = White,
+        )
+        Text(
+            modifier = Modifier.padding(top = 20.dp),
+            text = stringResource(R.string.nickname_title),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+        )
+        DiscordInputContainer(
+            modifier = Modifier.padding(top = 20.dp),
+            textState = nicknameState,
+            titleResId = R.string.alias_title,
+            textHintResId = R.string.signup_user_info,
+        )
+        DiscordRoundButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 15.dp),
+            textResId = R.string.next,
+            backgroundColor = Blue70,
+            onClick = { onClickConfirm(nicknameState.text.toString()) },
+            textColor = White,
+            radiusDp = 5.dp,
+            isEnable = nicknameState.text.isNotEmpty(),
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PreviewNicknameScreen() {
+    NicknameScreen(modifier = Modifier, onBackClick = {}, onClickConfirm = {})
+}
