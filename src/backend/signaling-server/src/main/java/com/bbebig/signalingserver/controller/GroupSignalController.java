02dfddb7a192c0d2888df95a9a00e1dd2f93cda8
soeun2537 <soeun2537@gmail.com>
2025-01-20 20:46:55 +0900

    feat: 그룹 시그널링 컨트롤러 구현

---
+++ src/backend/signaling-server/src/main/java/com/bbebig/signalingserver/controller/GroupSignalController.java
@@ -0,0 +1,31 @@
+package com.bbebig.signalingserver.controller;
+
+import com.bbebig.signalingserver.domain.SignalMessage;
+import com.bbebig.signalingserver.service.group.strategy.GroupSignalStrategy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 그룹 스트리밍 시그널링을 처리하는 컨트롤러
+ */
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class GroupSignalController {
+
+    private final GroupSignalStrategy groupSignalService;
+
+    @MessageMapping("/group")
+    public void onGroupSignal(@Payload SignalMessage message,
+                              @Header("simpSessionId") String sessionId) {
+
+        log.info("[Stream] 채널 타입: Group, 메시지 타입: {}, 보낸 사람 ID: {}, 채널 ID: {}",
+                message.getMessageType(), sessionId, message.getChannelId());
+
+        groupSignalService.processGroupSignal(message, sessionId);
+    }
+}
