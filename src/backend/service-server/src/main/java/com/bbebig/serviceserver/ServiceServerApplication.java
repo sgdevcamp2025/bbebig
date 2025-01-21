4f9914a506d7ddd88cc50a529e4dde07d4f9a73c
soeun2537 <soeun2537@gmail.com>
2025-01-21 15:51:39 +0900

    feat: env 설정 등록

--- src/backend/service-server/src/main/java/com/bbebig/serviceserver/ServiceServerApplication.java
+++ src/backend/service-server/src/main/java/com/bbebig/serviceserver/ServiceServerApplication.java
@@ -1,5 +1,6 @@
 package com.bbebig.serviceserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceServerApplication {
 
 	public static void main(String[] args) {
+		initEnv();
 		SpringApplication.run(ServiceServerApplication.class, args);
 	}
 
+	/**
+	 * 스트링부트 실행 전 시스템 property를 설정한다.
+	 */
+	static void initEnv() {
+		Dotenv.configure()
+				.directory("./src/main/resources/")
+				.filename(".env")
+				.load()
+				.entries()
+				.forEach(e -> {
+					System.setProperty(e.getKey(), e.getValue());
+				});
+	}
 }
