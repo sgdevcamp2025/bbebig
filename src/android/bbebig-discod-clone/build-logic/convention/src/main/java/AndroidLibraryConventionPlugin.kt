892b306a1920f7095d7a7e5a0cee2c9f17880e6a
seojeongwoo <qw4643987@naver.com>
2025-01-21 20:52:40 +0900

    feat: kotlin serialization 의존성 추가

--- src/android/bbebig-discod-clone/build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ src/android/bbebig-discod-clone/build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -1,8 +1,10 @@
 import com.android.build.gradle.LibraryExtension
 import com.smilegate.devcamp.bbebig.convention.configureKotlinAndroid
+import com.smilegate.devcamp.bbebig.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -10,11 +12,16 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.plugin.serialization")
             }
 
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
                 defaultConfig.targetSdk = 34
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("kotlinx.serialization.json").get())
             }
         }
     }
