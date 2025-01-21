77cfb56191ec7a2aca38b3abae5e3caed93bbd09
soeun2537 <soeun2537@gmail.com>
2025-01-21 16:03:56 +0900

    chore: 유레카 등록

--- src/backend/service-server/src/main/java/com/bbebig/serviceserver/ServiceServerApplication.java
+++ src/backend/service-server/src/main/java/com/bbebig/serviceserver/ServiceServerApplication.java
@@ -3,7 +3,11 @@ package com.bbebig.serviceserver;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceServerApplication {
 
