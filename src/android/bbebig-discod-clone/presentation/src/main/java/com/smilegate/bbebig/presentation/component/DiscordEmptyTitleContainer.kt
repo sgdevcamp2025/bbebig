0b1c31190d19fe7daf5d8157c69e3bf050c61938
seojeongwoo <qw4643987@naver.com>
2025-01-23 17:37:58 +0900

    feat: empty 타이틀 컴포넌트 생성

---
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/component/DiscordEmptyTitleContainer.kt
@@ -0,0 +1,23 @@
+package com.smilegate.bbebig.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DiscordEmptyTitleContainer(modifier: Modifier, firstTitleResId: Int, secondTitleResId: Int) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+    ) {
+        Text(stringResource(firstTitleResId), textAlign = TextAlign.Center)
+        Text(stringResource(secondTitleResId), textAlign = TextAlign.Center)
+    }
+}
