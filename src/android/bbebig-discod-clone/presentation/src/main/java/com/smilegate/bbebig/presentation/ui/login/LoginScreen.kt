da5e0233b3bd2b1931d62c55e391994b6b8f2856
seojeongwoo <qw4643987@naver.com>
2025-01-23 17:41:22 +0900

    feat: 안쓰는 import 제거

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/login/LoginScreen.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/login/LoginScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
