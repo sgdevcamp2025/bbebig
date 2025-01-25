711706c003b56961a46c42bc8f8552de3c4240ea
Zero-1016 <iolpd42@gmail.com>
2025-01-25 22:58:40 +0900

    refactor: help 로직을 수정한다.

--- src/backend/auth-server/src/libs/responseHelper.ts
+++ src/backend/auth-server/src/libs/responseHelper.ts
@@ -1,12 +1,13 @@
 import { FastifyReply } from 'fastify';
 import { CommonResponseType } from 'src/schema/type';
 
-function handleSuccess(res: FastifyReply, data: CommonResponseType, callback?: () => void) {
-  res.log.info(data);
-  res.status(200).send({
-    data,
-  });
-  callback?.();
+function handleSuccess(
+  res: FastifyReply,
+  data: CommonResponseType,
+  callback?: (res: FastifyReply) => void,
+) {
+  res.status(200).send(data);
+  callback?.(res);
 }
 
 export default handleSuccess;
