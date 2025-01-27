d141f708056c58f84f1de885269980801e885227
soeun2537 <soeun2537@gmail.com>
2025-01-27 17:04:01 +0900

    feat: 공통 응답 추가

---
+++ src/backend/apigateway-server/src/main/java/com/bbebig/apigatewayserver/global/response/code/success/BaseCode.java
@@ -0,0 +1,9 @@
+package com.bbebig.apigatewayserver.global.response.code.success;
+
+public interface BaseCode {
+
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+
+}
