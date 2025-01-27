d2f0325634227d3a546601a66e7367d10c05a39a
soeun2537 <soeun2537@gmail.com>
2025-01-27 17:21:19 +0900

    feat: Passport를 발급하는 PassportGenerator 구현

---
+++ src/backend/apigateway-server/src/main/java/com/bbebig/apigatewayserver/passport/PassportGenerator.java
@@ -0,0 +1,40 @@
+package com.bbebig.apigatewayserver.passport;
+
+import com.bbebig.common.proto.PassportProto.Passport;
+import java.time.Instant;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Passport를 발급
+ */
+@Service
+public class PassportGenerator {
+
+    @Value("${eas.passport.expiration}")
+    private static long EXPIRE_SECONDS;
+
+    /**
+     * 사용자 정보를 받아 Passport 생성 + HMAC 서명 + Base64
+     */
+    public String generatePassport(long userId, String authLevel) {
+        long now = Instant.now().getEpochSecond();
+        long expire = now + EXPIRE_SECONDS;
+
+        // Protobuf 객체 생성
+        Passport passport = Passport.newBuilder()
+                .setPassportId(UUID.randomUUID().toString())
+                .setUserId(userId)
+                .setIssuedAt(now)
+                .setExpiresAt(expire)
+                .setAuthLevel(authLevel)
+                .build();
+
+        // 직렬화 -> byte[]
+        byte[] rawBytes = passport.toByteArray();
+
+        // HMAC 서명 + Base64
+        return HmacUtil.signPassport(rawBytes);
+    }
+}
