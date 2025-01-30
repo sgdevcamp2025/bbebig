6ea79d8edcdc694ed6500eb9c5acc906fb2dd1b1
이소은 <soeun2537@gmail.com>
2025-01-30 11:09:43 +0900

    [BE] API Gateway Passport 발급, 검증 구현 및 @PassportUser 어노테이션 구현 #81 (#110)
    * chore: common-domain 초기 세팅
    
    * feat: common-domain에 공통 응답 추가
    
    * feat: Passport를 발급하는 PassportGenerator 구현
    
    * feat: Passport Protocol Buffer 추가
    
    * feat: Passport를 검증하는 PassportValidator 구현
    
    * feat: Passport를 헤더에서 추출하는 PassportExtractor 구현
    
    * feat: Hmac 서명, 검증 및 추출하는 HmacUtil 구현
    
    * chore: dotenv 의존성 추가 및 초기 세팅
    
    * chore: protobuf common-domain 추가
    
    * feat: Passport 기반 Spring Cloud Global Gateway 구현
    
    * refactor: common-domain 이름 common-module로 변경
    
    * refactor: common-module passport 폴더 구조 변경
    
    * feat: BaseTimeEntity 공통 상속 클래스 추가
    
    * feat: @PassportUser 어노테이션 및 resolver 구현

---
+++ src/backend/common-module/src/main/java/com/bbebig/commonmodule/passport/resolver/PassportArgumentResolver.java
@@ -0,0 +1,50 @@
+package com.bbebig.commonmodule.passport.resolver;
+
+import com.bbebig.commonmodule.passport.PassportValidator;
+import com.bbebig.commonmodule.passport.annotation.PassportUser;
+import com.bbebig.commonmodule.proto.PassportProto.Passport;
+import com.bbebig.commonmodule.global.response.code.error.ErrorStatus;
+import com.bbebig.commonmodule.global.response.exception.ErrorHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class PassportArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final PassportValidator passportValidator;
+
+    @Value("${eas.passport.header}")
+    private String PASSPORT_HEADER;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(PassportUser.class)
+                && parameter.getParameterType().equals(Passport.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String base64Passport = request.getHeader(PASSPORT_HEADER);
+        if (base64Passport == null) {
+            throw new ErrorHandler(ErrorStatus.PASSPORT_HEADER_MISSING);
+        }
+
+        try {
+            return passportValidator.validatePassport(base64Passport);
+        } catch (Exception e) {
+            throw new ErrorHandler(ErrorStatus.PASSPORT_INVALID_OR_TAMPERED);
+        }
+    }
+}
