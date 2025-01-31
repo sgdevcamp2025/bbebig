f7d9e4750281083950b3c901de849529059abc60
Jihyeong <iolpd42@gmail.com>
2025-01-31 13:38:56 +0900

    [BE] Docker 환경 설정 및 빌드 프로세스 개선 #90 (#123)
    * fix: docker 연결을 해결합니다.
    
    * fix: 불필요한 로그제거
    
    * docs: 문서확인 명령어를 추가합니다.
    
    * docs: env 예시 파일 추가

--- src/backend/auth-server/src/libs/redis.ts
+++ src/backend/auth-server/src/libs/redis.ts
@@ -1,5 +1,22 @@
 import Redis from 'ioredis';
+import { REDIS_HOST, REDIS_PORT } from './constants';
 
-const redis = new Redis();
+const redis = new Redis({
+  host: REDIS_HOST,
+  port: Number(REDIS_PORT),
+  maxRetriesPerRequest: 3,
+  retryStrategy(times) {
+    const delay = Math.min(times * 50, 2000);
+    return delay;
+  },
+  enableReadyCheck: true,
+  reconnectOnError(err) {
+    const targetError = 'READONLY';
+    if (err.message.includes(targetError)) {
+      return true;
+    }
+    return false;
+  },
+});
 
 export default redis;
