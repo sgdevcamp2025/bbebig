1db01bfcc6c5acb1d852e8008ef6b43a17b1997a
dh1010a <dh1010a@naver.com>
2025-01-31 20:05:27 +0900

    feat: 서버별 라우팅 정보 설정

--- src/backend/apigateway-server/src/main/java/com/bbebig/apigatewayserver/ApigatewayServerApplication.java
+++ src/backend/apigateway-server/src/main/java/com/bbebig/apigatewayserver/ApigatewayServerApplication.java
@@ -3,9 +3,10 @@ package com.bbebig.apigatewayserver;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class ApigatewayServerApplication {
 
