1acda00df6b443dd195b34b471b39f8fca5de6d4
dh1010a <dh1010a@naver.com>
2025-01-31 15:11:18 +0900

    refactor: presence dto 타입 변경

--- src/backend/common-module/src/main/java/com/bbebig/commonmodule/kafka/dto/PresenceEventDto.java
+++ src/backend/common-module/src/main/java/com/bbebig/commonmodule/kafka/dto/PresenceEventDto.java
@@ -4,11 +4,13 @@ import com.bbebig.commonmodule.kafka.dto.model.PresenceType;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class PresenceEventDto {
 	private Long memberId;
 	private PresenceType globalStatus;
 	private PresenceType actualStatus;
-	private String lastActivityTime;
+	private LocalDateTime lastActivityTime;
 }
