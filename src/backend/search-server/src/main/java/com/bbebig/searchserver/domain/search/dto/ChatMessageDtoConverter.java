b37483b88cdac6f53f76502ca25df9ea24e361be
이소은 <soeun2537@gmail.com>
2025-02-01 20:47:51 +0900

    [BE] 검색 서버 메인으로 병합 (#122) (#135)
    * feat: 수신 받은 메시지를 MongoDB에 저장하는 기능 구현
    
    * feat: 수신 받은 메시지를 엘라스틱 서치에 저장하는 기능 구현
    
    * refactor: 엘라스틱 서치에 저장되는 데이터 형식 변경
    
    * feat: 기본 내용 기반 검색 기능 구현
    
    * feat: 다중 옵션을 포함한 검색이 가능하도록 기능 구현
    
    * feat: 메시지 삭제 및 수정 기능 구현
    
    * refactor: 검색 옵션 검증 로직 추가
    
    * fix: 날짜 검색 옵션 별로 날짜 다르게 받아오도록 수정
    
    * fix: 몽고 db 및 dotenv 설정 수정
    
    * refactor: 사용하지 않는 dto 제거
    
    Co-authored-by: Paek Do Hyun <dh1010a@naver.com>

---
+++ src/backend/search-server/src/main/java/com/bbebig/searchserver/domain/search/dto/ChatMessageDtoConverter.java
@@ -0,0 +1,60 @@
+package com.bbebig.searchserver.domain.search.dto;
+
+import com.bbebig.searchserver.domain.search.domain.ChannelChatMessage;
+import com.bbebig.searchserver.domain.search.domain.ChannelChatMessageElastic;
+import com.bbebig.searchserver.domain.search.domain.DmChatMessage;
+import com.bbebig.searchserver.domain.search.domain.DmChatMessageElastic;
+import com.bbebig.searchserver.global.kafka.dto.ChatMessageDto;
+
+public class ChatMessageDtoConverter {
+
+	public static ChannelChatMessage convertDtoToChannelChatMessage(ChatMessageDto messageDto) {
+		return ChannelChatMessage.builder()
+				.id(messageDto.getId())
+				.serverId(messageDto.getServerId())
+				.channelId(messageDto.getChannelId())
+				.sendMemberId(messageDto.getSendMemberId())
+				.content(messageDto.getContent())
+				.attachedFiles(messageDto.getAttachedFiles())
+				.createdAt(messageDto.getCreatedAt() != null ? messageDto.getCreatedAt() : null)
+				.updatedAt(messageDto.getType().equals("MESSAGE_UPDATE") ? messageDto.getUpdatedAt() : null)
+				.messageType(messageDto.getMessageType())
+				.build();
+	}
+
+	public static DmChatMessage convertDtoToDmChatMessage(ChatMessageDto messageDto) {
+		return DmChatMessage.builder()
+				.id(messageDto.getId())
+				.channelId(messageDto.getChannelId())
+				.sendMemberId(messageDto.getSendMemberId())
+				.content(messageDto.getContent())
+				.attachedFiles(messageDto.getAttachedFiles())
+				.targetMemberIds(messageDto.getTargetMemberIds())
+				.createdAt(messageDto.getCreatedAt() != null ? messageDto.getCreatedAt() : null)
+				.updatedAt(messageDto.getType().equals("MESSAGE_UPDATE") ? messageDto.getUpdatedAt() : null)
+				.messageType(messageDto.getMessageType())
+				.build();
+	}
+
+	public static ChannelChatMessageElastic convertDtoToChannelChatMessageElastic(ChatMessageDto messageDto) {
+		return ChannelChatMessageElastic.builder()
+				.id(messageDto.getId())
+				.serverId(messageDto.getServerId())
+				.channelId(messageDto.getChannelId())
+				.sendMemberId(messageDto.getSendMemberId())
+				.content(messageDto.getContent())
+				.createdAt(messageDto.getCreatedAt() != null ? messageDto.getCreatedAt() : null)
+				.build();
+	}
+
+	public static DmChatMessageElastic convertDtoToDmChatMessageElastic(ChatMessageDto messageDto) {
+		return DmChatMessageElastic.builder()
+				.id(messageDto.getId())
+				.channelId(messageDto.getChannelId())
+				.sendMemberId(messageDto.getSendMemberId())
+				.content(messageDto.getContent())
+				.createdAt(messageDto.getCreatedAt() != null ? messageDto.getCreatedAt() : null)
+				.build();
+	}
+
+}
