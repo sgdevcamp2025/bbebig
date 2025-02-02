659bc241d5dadae5f564493b1e892d652f820dc9
Zero-1016 <iolpd42@gmail.com>
2025-02-03 00:41:38 +0900

    chore: storybook용 vitejs/plugin-react를 설치합니다.

--- src/frontend/storybook-with-vite.config.ts
+++ src/frontend/storybook-with-vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   process.env = { ...process.env, ...env }
 
   return {
-    plugins: [tsconfigPaths()]
+    plugins: [tsconfigPaths(), react()]
   }
 })
