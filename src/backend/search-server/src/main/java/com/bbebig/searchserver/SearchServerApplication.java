e1f8938476205a3de01848eecfcc65843be7fada
soeun2537 <soeun2537@gmail.com>
2025-02-03 23:30:03 +0900

    feat: dotenv 설정

--- src/backend/search-server/src/main/java/com/bbebig/searchserver/SearchServerApplication.java
+++ src/backend/search-server/src/main/java/com/bbebig/searchserver/SearchServerApplication.java
@@ -1,5 +1,6 @@
 package com.bbebig.searchserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -12,7 +13,21 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class SearchServerApplication {
 
 	public static void main(String[] args) {
+		initEnv();
 		SpringApplication.run(SearchServerApplication.class, args);
 	}
 
+	/**
+	 * 스트링부트 실행 전 시스템 property를 설정한다.
+	 */
+	static void initEnv() {
+		Dotenv.configure()
+				.directory("./src/main/resources/")
+				.filename(".env")
+				.load()
+				.entries()
+				.forEach(e -> {
+					System.setProperty(e.getKey(), e.getValue());
+				});
+	}
 }
