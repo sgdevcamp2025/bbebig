0b9d3820a22cac3eaa08c8a3feefc0c11f64b66f
soeun2537 <soeun2537@gmail.com>
2025-02-03 22:20:30 +0900

    feat: application.yml 추가

--- src/backend/push-server/src/main/java/com/bbebig/push_server/PushServerApplication.java
+++ src/backend/push-server/src/main/java/com/bbebig/push_server/PushServerApplication.java
@@ -2,6 +2,7 @@ package com.bbebig.push_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PushServerApplication {
