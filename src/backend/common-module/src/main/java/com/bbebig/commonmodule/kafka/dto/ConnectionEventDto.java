3f8aef3f4593b52f8557790dd67cfbbbdd6ac35e
dh1010a <dh1010a@naver.com>
2025-02-06 17:19:27 +0900

    fix: jsonCreator 생성

--- src/backend/common-module/src/main/java/com/bbebig/commonmodule/kafka/dto/ConnectionEventDto.java
+++ src/backend/common-module/src/main/java/com/bbebig/commonmodule/kafka/dto/ConnectionEventDto.java
@@ -1,6 +1,8 @@
 package com.bbebig.commonmodule.kafka.dto;
 
 import com.bbebig.commonmodule.kafka.dto.model.ChannelType;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -27,5 +29,28 @@ public class ConnectionEventDto {
 	private Long currentChannelId;
 
 	private Long currentServerId;
+
+	@JsonCreator
+	public ConnectionEventDto(
+			@JsonProperty("memberId") Long memberId,
+			@JsonProperty("type") String type,
+			@JsonProperty("socketSessionId") String socketSessionId,
+			@JsonProperty("connectedServerIp") String connectedServerIp,
+			@JsonProperty("platform") String platform,
+			@JsonProperty("deviceType") String deviceType,
+			@JsonProperty("currentChannelType") ChannelType currentChannelType,
+			@JsonProperty("currentChannelId") Long currentChannelId,
+			@JsonProperty("currentServerId") Long currentServerId
+	) {
+		this.memberId = memberId;
+		this.type = type;
+		this.socketSessionId = socketSessionId;
+		this.connectedServerIp = connectedServerIp;
+		this.platform = platform;
+		this.deviceType = deviceType;
+		this.currentChannelType = currentChannelType;
+		this.currentChannelId = currentChannelId;
+		this.currentServerId = currentServerId;
+	}
 
 }
--- src/backend/common-module/src/main/java/com/bbebig/commonmodule/kafka/dto/PresenceEventDto.java
+++ src/backend/common-module/src/main/java/com/bbebig/commonmodule/kafka/dto/PresenceEventDto.java
@@ -1,6 +1,8 @@
 package com.bbebig.commonmodule.kafka.dto;
 
 import com.bbebig.commonmodule.kafka.dto.model.PresenceType;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,4 +15,15 @@ public class PresenceEventDto {
 	private PresenceType globalStatus;
 	private PresenceType actualStatus;
 	private LocalDateTime lastActivityTime;
+
+	@JsonCreator
+	public PresenceEventDto(@JsonProperty("memberId") Long memberId,
+							@JsonProperty("globalStatus") PresenceType globalStatus,
+							@JsonProperty("actualStatus") PresenceType actualStatus,
+							@JsonProperty("lastActivityTime") LocalDateTime lastActivityTime) {
+		this.memberId = memberId;
+		this.globalStatus = globalStatus;
+		this.actualStatus = actualStatus;
+		this.lastActivityTime = lastActivityTime;
+	}
 }
