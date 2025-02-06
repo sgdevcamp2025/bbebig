7834acf48f69e251ecb199cc0dfe6ab21031dd7f
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-02-06 15:16:40 +0900

    feat: 친구 탭 이동 기능

---
+++ src/frontend/src/pages/dm/components/header.tsx
@@ -0,0 +1,68 @@
+const navigation = [
+  {
+    name: '온라인',
+    component: 'ONLINE'
+  },
+  {
+    name: '모두',
+    component: 'ALL'
+  },
+  {
+    name: '차단',
+    component: 'BLOCK'
+  },
+  {
+    name: '대기 중',
+    component: 'PENDING'
+  },
+  {
+    name: '차단 목록',
+    component: 'BLOCK_FRIENDS'
+  }
+] as const
+
+type TabType = (typeof navigation)[number]['component']
+
+interface HeaderProps {
+  onTabChange: (tab: TabType) => void
+  currentTab: TabType
+}
+
+function Header({ onTabChange, currentTab }: HeaderProps) {
+  return (
+    <div className='flex items-center px-3 py-3 border-b border-discord-gray-800'>
+      <div className='flex items-center gap-2'>
+        <img
+          src='/icon/dm/friend.svg'
+          alt='친구'
+          className='w-[20px] h-[20px]'
+        />
+        <span className='text-discord-font-color-normal text-lg font-semibold'>친구</span>
+        <div className='h-5 border-r border-discord-gray-500 mr-4' />
+      </div>
+      <div className='flex items-center gap-5'>
+        {navigation.map((item) => (
+          <button
+            type='button'
+            key={item.component}
+            onClick={() => onTabChange(item.component)}
+            className={`px-2 py-1 rounded-md
+             hover:bg-discord-gray-600 hover:text-discord-font-color-normal 
+             text-discord-font-color-muted
+             text-base font-medium 
+             cursor-pointer
+             ${currentTab === item.component ? 'bg-discord-gray-600 text-discord-font-color-normal' : ''}`}>
+            {item.name}
+          </button>
+        ))}
+      </div>
+      <button
+        type='button'
+        className='ml-3 px-1 py-0.5 rounded-md bg-[#3BA55D] text-sm text-white font-medium'>
+        친구 추가하기
+      </button>
+    </div>
+  )
+}
+
+export default Header
