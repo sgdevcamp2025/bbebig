d061df356ad47c5591034022ef5802d8cbc404cf
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-02-09 16:44:19 +0900

    refactor: useEffect 삭제하여 불필요한 connect 방지

--- src/frontend/src/hooks/use-web-socket.ts
+++ src/frontend/src/hooks/use-web-socket.ts
@@ -1,15 +1,7 @@
 import { useWebSocketStore } from '@/stores/use-web-socket-store'
-import { useEffect } from 'react'
 
 export const useWebSocket = () => {
   const { connect, disconnect, sendMessage, subscribe, isConnected } = useWebSocketStore()
-
-  useEffect(() => {
-    connect()
-    return () => {
-      disconnect()
-    }
-  }, [])
 
   return {
     connect,
