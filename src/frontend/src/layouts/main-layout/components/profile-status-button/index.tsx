78cdccbdf54389553604583a56bf9f472c2cc057
Zero-1016 <iolpd42@gmail.com>
2025-02-09 22:41:02 +0900

    refactor: hydration 문제 개선

--- src/frontend/src/layouts/main-layout/components/profile-status-button/index.tsx
+++ src/frontend/src/layouts/main-layout/components/profile-status-button/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
 
 interface ProfileStatusButtonProps {
   'aria-label': string
@@ -15,11 +15,13 @@ const ProfileStatusButton = ({
 }: ProfileStatusButtonProps) => {
   const [isClient, setIsClient] = useState(false)
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setIsClient(true)
   }, [])
 
-  const iconSuffix = isClient ? (isMuted ? '-muted' : '') : ''
+  if (!isClient) return null
+
+  const iconSuffix = isMuted ? '-muted' : ''
   const iconPath = `/icon/menu/${icon}${iconSuffix}.svg`
 
   return (
@@ -28,12 +30,10 @@ const ProfileStatusButton = ({
       onClick={onClick}
       className='w-8 h-8 flex items-center justify-center rounded-md hover:bg-gray-80'
       aria-label={ariaLabel}>
-      {isClient && ( // 클라이언트 사이드에서만 이미지 렌더링
-        <img
-          src={iconPath}
-          className='w-5 h-5'
-        />
-      )}
+      <img
+        src={iconPath}
+        className='w-5 h-5'
+      />
     </button>
   )
 }
