6a7e586abc26437d36072e2a48d0c6e90e111138
Zero-1016 <iolpd42@gmail.com>
2025-02-09 12:29:31 +0900

    fix: hydration 에러 수정

--- src/frontend/src/layouts/main-layout/components/profile-status-button/index.tsx
+++ src/frontend/src/layouts/main-layout/components/profile-status-button/index.tsx
@@ -1,23 +1,39 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { useEffect, useState } from 'react'
 
-type Props = {
+interface ProfileStatusButtonProps {
+  'aria-label': string
   onClick: () => void
-  icon: string
   isMuted: boolean
-} & ComponentPropsWithoutRef<'button'>
+  icon: string
+}
 
-const ProfileStatusButton = ({ onClick, icon, isMuted, ...props }: Props) => {
+const ProfileStatusButton = ({
+  onClick,
+  'aria-label': ariaLabel,
+  isMuted,
+  icon
+}: ProfileStatusButtonProps) => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  const iconSuffix = isClient ? (isMuted ? '-muted' : '') : ''
+  const iconPath = `/icon/menu/${icon}${iconSuffix}.svg`
+
   return (
     <button
       type='button'
       onClick={onClick}
       className='w-8 h-8 flex items-center justify-center rounded-md hover:bg-gray-80'
-      {...props}>
-      <img
-        src={`/icon/menu/${icon}${isMuted ? '-muted' : ''}.svg`}
-        alt={icon}
-        className='w-5 h-5'
-      />
+      aria-label={ariaLabel}>
+      {isClient && ( // 클라이언트 사이드에서만 이미지 렌더링
+        <img
+          src={iconPath}
+          className='w-5 h-5'
+        />
+      )}
     </button>
   )
 }
