2a888d13708740e580ce23eab618edc8477d2434
soeun2537 <soeun2537@gmail.com>
2025-02-11 18:53:57 +0900

    refactor: 친구 update API 예외 처리 및 리팩토링

--- src/backend/user-server/src/main/java/com/bbebig/userserver/friend/repository/FriendRepository.java
+++ src/backend/user-server/src/main/java/com/bbebig/userserver/friend/repository/FriendRepository.java
@@ -3,15 +3,22 @@ package com.bbebig.userserver.friend.repository;
 import com.bbebig.userserver.friend.entity.Friend;
 import com.bbebig.userserver.friend.entity.FriendStatus;
 import com.bbebig.userserver.member.entity.Member;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface FriendRepository extends JpaRepository<Friend,Long> {
-    Optional<Friend> findByStatusNot(FriendStatus status);
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+    @Query("SELECT CASE WHEN f.fromMember.id = :memberId THEN f.toMember ELSE f.fromMember END " +
+            "FROM Friend f WHERE (f.fromMember.id = :memberId OR f.toMember.id = :memberId) " +
+            "AND f.friendStatus = :status")
+    List<Member> findFriendMembersByMemberIdAndStatus(@Param("memberId") Long memberId,
+                                                      @Param("status") FriendStatus status);
 
-    Page<Friend> findByToMemberAndStatus(Member member, FriendStatus status, Pageable pageable);
-    Page<Friend> findByStatusAndToMemberOrFromMember( FriendStatus status,Member toMember, Member fromMember, Pageable pageable);
+    @Query("SELECT f FROM Friend f WHERE (f.fromMember = :member1 AND f.toMember = :member2) " +
+            "OR (f.fromMember = :member2 AND f.toMember = :member1)")
+    Optional<Friend> findFriendMember1AndMember2(@Param("member1") Member member1,
+                                        @Param("member2") Member member2);
 }
--- src/backend/user-server/src/main/java/com/bbebig/userserver/friend/service/FriendService.java
+++ src/backend/user-server/src/main/java/com/bbebig/userserver/friend/service/FriendService.java
@@ -1,57 +1,182 @@
 package com.bbebig.userserver.friend.service;
 
+import com.bbebig.commonmodule.global.response.code.error.ErrorStatus;
+import com.bbebig.commonmodule.global.response.exception.ErrorHandler;
+import com.bbebig.userserver.friend.dto.response.*;
 import com.bbebig.userserver.friend.entity.Friend;
 import com.bbebig.userserver.friend.entity.FriendStatus;
 import com.bbebig.userserver.friend.repository.FriendRepository;
 import com.bbebig.userserver.member.entity.Member;
 import com.bbebig.userserver.member.repository.MemberRepository;
-import com.bbebig.userserver.friend.dto.request.FriendCreateRequest;
-import com.bbebig.userserver.friend.dto.response.FriendCreateResponse;
-import com.bbebig.userserver.friend.dto.response.FriendResponse;
-import jakarta.transaction.Transactional;
+import com.bbebig.userserver.friend.dto.request.FriendCreateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class FriendService {
+
     private final FriendRepository friendRepository;
     private final MemberRepository memberRepository;
 
-    public void createFriend(Long memberId, FriendCreateRequest request) {
-        //수락상태가 아닌거만 조회
-        Optional<Friend> existFriend = friendRepository.findByStatusNot(FriendStatus.ACCEPTED);
-        if (existFriend.isPresent()) {
-            //있으면 pending으로 변경
-            existFriend.get().changeFriendStatus(FriendStatus.PENDING);
-        } else {
-            //없으면 생성
-            Member fromMember = memberRepository.findById(memberId).get();
-            Member toMember = memberRepository.findById(request.toMemberId()).get();
-            friendRepository.save(Friend.of(fromMember, toMember));
+    /**
+     * 친구 요청 (생성)
+     */
+    @Transactional
+    public FriendCreateResponseDto createFriend(Long memberId, FriendCreateRequestDto friendCreateRequestDto) {
+        Member fromMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        Member toMember = memberRepository.findById(friendCreateRequestDto.getToMemberId())
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        // 본인에게 요청을 보낸 경우
+        if (fromMember.equals(toMember)) {
+            throw new ErrorHandler(ErrorStatus.FRIEND_REQUEST_SELF);
+        }
+
+        Optional<Friend> optionalFriend = friendRepository.findFriendMember1AndMember2(fromMember, toMember);
+
+        // 친구 관계가 이미 존재할 때
+        if (optionalFriend.isPresent()) {
+            Friend friend = optionalFriend.get();
+
+            if (friend.getFriendStatus() == FriendStatus.DECLINED) {
+                friend.updateFriendStatus(FriendStatus.PENDING);
+                return FriendCreateResponseDto.convertToFriendCreateResponseDto(friend);
+            }
+            throw new ErrorHandler(ErrorStatus.FRIEND_RELATION_EXIST);
+        }
+
+        Friend friend = Friend.builder()
+                .fromMember(fromMember)
+                .toMember(toMember)
+                .friendStatus(FriendStatus.PENDING)
+//                .serverList() TODO: 같이 있는 서버 조회 (FeignClient)
+                .build();
+
+        friendRepository.save(friend);
+
+        return FriendCreateResponseDto.convertToFriendCreateResponseDto(friend);
+    }
+
+    /**
+     * 요청 대기 중인 친구 목록 조회
+     */
+    @Transactional(readOnly = true)
+    public List<FriendListResponseDto> getPendingFriends(Long memberId) {
+        memberRepository.findById(memberId)
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        return friendRepository.findFriendMembersByMemberIdAndStatus(memberId, FriendStatus.PENDING).stream()
+                .map(FriendListResponseDto::convertToFriendListResponseDto)
+                .toList();
+    }
+
+    /**
+     * 요청 수락한 친구 목록 조회
+     */
+    @Transactional(readOnly = true)
+    public List<FriendListResponseDto> getAcceptedFriends(Long memberId) {
+        memberRepository.findById(memberId)
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        return friendRepository.findFriendMembersByMemberIdAndStatus(memberId, FriendStatus.ACCEPTED).stream()
+                .map(FriendListResponseDto::convertToFriendListResponseDto)
+                .toList();
+    }
+
+    /**
+     * 친구 요청 수락/거절
+     */
+    @Transactional
+    public FriendUpdateResponseDto updateFriendStatus(Long memberId, Long friendId, FriendStatus friendStatus) {
+        Friend friend = friendRepository.findById(friendId)
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.FRIEND_NOT_FOUND));
+
+        // 친구 요청을 받은 사람이 본인인지 확인
+        validateToMember(memberId, friend);
+
+        // 친구 요청 상태가 아닐 경우
+        if (friend.getFriendStatus() != FriendStatus.PENDING) {
+            throw new ErrorHandler(ErrorStatus.FRIEND_REQUEST_NOT_FOUND);
+        }
+
+        friend.updateFriendStatus(friendStatus);
+
+        return FriendUpdateResponseDto.convertToFriendUpdateResponseDto(friend);
+    }
+
+    /**
+     * 친구 삭제
+     */
+    @Transactional
+    public FriendDeleteResponseDto deleteFriend(Long memberId, Long friendId) {
+        Friend friend = friendRepository.findById(friendId)
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.FRIEND_NOT_FOUND));
+
+        // 본인의 친구 관계인지 확인
+        checkOwnRelation(memberId, friend);
+
+        // 친구가 아닐 경우
+        if (friend.getFriendStatus() != FriendStatus.ACCEPTED) {
+            throw new ErrorHandler(ErrorStatus.FRIEND_RELATION_NOT_EXIST);
+        }
+
+        friendRepository.delete(friend);
+
+        return FriendDeleteResponseDto.convertToFriendDeleteResponseDto(friend);
+    }
+
+    /**
+     * 친구 요청 취소
+     */
+    @Transactional
+    public FriendDeleteResponseDto cancelFriendPending(Long memberId, Long friendId) {
+        Friend friend = friendRepository.findById(friendId)
+                .orElseThrow(() -> new ErrorHandler(ErrorStatus.FRIEND_NOT_FOUND));
+
+        // 친구 요청을 보낸 사람이 본인인지 확인
+        validateFromMember(memberId, friend);
+
+        // 친구 요청을 보낸 상태가 아닐 경우
+        if (friend.getFriendStatus() != FriendStatus.PENDING) {
+            throw new ErrorHandler(ErrorStatus.FRIEND_REQUEST_NOT_PENDING);
+        }
+
+        friendRepository.delete(friend);
+
+        return FriendDeleteResponseDto.convertToFriendDeleteResponseDto(friend);
+    }
+
+    /**
+     * 본인의 친구 관계인지 확인
+     */
+    private void checkOwnRelation(Long memberId, Friend friend) {
+        if (!friend.getFromMember().getId().equals(memberId) && !friend.getToMember().getId().equals(memberId)) {
+            throw new ErrorHandler(ErrorStatus.UNAUTHORIZED_FRIEND_ACTION);
         }
     }
 
-    public Page<FriendCreateResponse> getFriendCreate(Long memberId, Pageable pageRequest) {
-        Member member = memberRepository.findById(memberId).get();
-        return friendRepository.findByToMemberAndStatus(member, FriendStatus.PENDING, pageRequest).map(FriendCreateResponse::of);
+    /**
+     * 친구 요청을 받은 사람이 본인인지 확인
+     */
+    private void validateToMember(Long memberId, Friend friend) {
+        if (!friend.getToMember().getId().equals(memberId)) {
+            throw new ErrorHandler(ErrorStatus.UNAUTHORIZED_FRIEND_ACTION);
+        }
     }
 
-    public Page<FriendResponse> getFriendList(Long memberId, Pageable pageRequest) {
-        Member member = memberRepository.findById(memberId).get();
-        return friendRepository.findByStatusAndToMemberOrFromMember(FriendStatus.ACCEPTED, member, member, pageRequest).map(FriendResponse::of);
-    }
-
-    public void changeFriendCreateStatus(Long friendId, FriendStatus requestStatus) {
-        friendRepository.findById(friendId).get().changeFriendStatus(requestStatus);
-    }
-
-    public void deleteFriend(Long friendId) {
-        friendRepository.findById(friendId).get().changeFriendStatus(FriendStatus.DELETED);
+    /**
+     * 친구 요청을 보낸 사람이 본인인지 확인
+     */
+    private void validateFromMember(Long memberId, Friend friend) {
+        if (!friend.getFromMember().getId().equals(memberId)) {
+            throw new ErrorHandler(ErrorStatus.UNAUTHORIZED_FRIEND_ACTION);
+        }
     }
 }
