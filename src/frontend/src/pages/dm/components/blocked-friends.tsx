e4029f4e9548b06351e2d89a4cc6d540306c9266
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-02-11 11:40:25 +0900

    feat: 차단 목록 검색바 추가

--- src/frontend/src/pages/dm/components/blocked-friends.tsx
+++ src/frontend/src/pages/dm/components/blocked-friends.tsx
@@ -1,5 +1,16 @@
+import SearchInput from '@/components/search-input'
+
 function BlockedFriends() {
-  return <div>BlockedFriends</div>
+  return (
+    <div className='flex flex-col p-4'>
+      <SearchInput
+        onSearch={() => {
+          console.log('검색')
+        }}
+        placeholder='검색'
+      />
+    </div>
+  )
 }
 
 export default BlockedFriends
