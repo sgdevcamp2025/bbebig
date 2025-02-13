3b19a01ba4b2e157f78f085404a06dddafd8bd3e
soeun2537 <soeun2537@gmail.com>
2025-02-13 18:07:34 +0900

    chore: passport-server 초기 설정

---
+++ src/backend/passport-server/src/main/java/com/bbebig/passportserver/PassportServerApplication.java
@@ -0,0 +1,33 @@
+package com.bbebig.passportserver;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PassportServerApplication {
+
+	public static void main(String[] args) {
+		initEnv();
+		SpringApplication.run(PassportServerApplication.class, args);
+	}
+
+	/**
+	 * 스트링부트 실행 전 시스템 property를 설정한다.
+	 */
+	static void initEnv() {
+		Dotenv.configure()
+				.directory("./src/main/resources/")
+				.filename(".env")
+				.load()
+				.entries()
+				.forEach(e -> {
+					System.setProperty(e.getKey(), e.getValue());
+				});
+	}
+}
