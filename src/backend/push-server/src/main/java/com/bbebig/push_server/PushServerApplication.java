7c3a895479544d917d239b8e05cd2383651a56fd
Paek Do Hyun <dh1010a@naver.com>
2025-02-13 21:38:12 +0900

    [BE] 푸시 서버 메인으로 병합 (#234)
    * feat: application.yml 추가
    
    * fix: datasource 강제 끄기
    
    * feat: application.yml 추가
    
    * chore: dotenv 의존성 추가
    
    * chore: validation 의존성 수정
    
    ---------
    
    Co-authored-by: soeun2537 <soeun2537@gmail.com>

--- src/backend/push-server/src/main/java/com/bbebig/push_server/PushServerApplication.java
+++ src/backend/push-server/src/main/java/com/bbebig/push_server/PushServerApplication.java
@@ -1,13 +1,33 @@
 package com.bbebig.push_server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PushServerApplication {
 
 	public static void main(String[] args) {
+		initEnv();
 		SpringApplication.run(PushServerApplication.class, args);
 	}
 
+	/**
+	 * 스트링부트 실행 전 시스템 property를 설정한다.
+	 */
+	static void initEnv() {
+		Dotenv.configure()
+				.directory("./src/main/resources/")
+				.filename(".env")
+				.load()
+				.entries()
+				.forEach(e -> {
+					System.setProperty(e.getKey(), e.getValue());
+				});
+	}
 }
