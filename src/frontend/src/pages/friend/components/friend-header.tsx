b5f5f1e95cd8a4b1ff002cb8d3bd96ab7950d895
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-02-13 17:37:15 +0900

    chore: 빈 태그 삭제

--- src/frontend/src/pages/friend/components/friend-header.tsx
+++ src/frontend/src/pages/friend/components/friend-header.tsx
@@ -26,37 +26,34 @@ interface HeaderProps {
 function FriendHeader({ onTabChange, currentTab }: HeaderProps) {
   return (
     <CommonHeader type='FRIEND'>
-      <>
-        <img
-          src='/icon/dm/friend.svg'
-          alt='친구'
-          className='w-[20px] h-[20px]'
-        />
-        <span className='text-discord-font-color-normal text-lg font-semibold'>친구</span>
-        <div className='h-5 border-r border-discord-gray-500 mr-4' />
+      <img
+        src='/icon/dm/friend.svg'
+        alt='친구'
+        className='w-[20px] h-[20px]'
+      />
+      <span className='text-discord-font-color-normal text-lg font-semibold'>친구</span>
+      <div className='h-5 border-r border-discord-gray-500 mr-4' />
 
-        <div className='flex items-center gap-5'>
-          {navigation.map((item) => (
-            <button
-              type='button'
-              key={item.component}
-              onClick={() => onTabChange(item.component)}
-              className={cn(
-                'px-1 py-0.5 rounded-md text-base font-medium cursor-pointer',
-                'hover:bg-discord-gray-500 hover:text-discord-font-color-normal text-discord-font-color-muted',
-                currentTab === item.component &&
-                  'bg-discord-gray-500 text-discord-font-color-normal'
-              )}>
-              {item.name}
-            </button>
-          ))}
-        </div>
-        <button
-          type='button'
-          className='ml-5 px-1 py-0.5 rounded-md bg-[#248045] text-base text-white font-medium'>
-          친구 추가하기
-        </button>
-      </>
+      <div className='flex items-center gap-5'>
+        {navigation.map((item) => (
+          <button
+            type='button'
+            key={item.component}
+            onClick={() => onTabChange(item.component)}
+            className={cn(
+              'px-1 py-0.5 rounded-md text-base font-medium cursor-pointer',
+              'hover:bg-discord-gray-500 hover:text-discord-font-color-normal text-discord-font-color-muted',
+              currentTab === item.component && 'bg-discord-gray-500 text-discord-font-color-normal'
+            )}>
+            {item.name}
+          </button>
+        ))}
+      </div>
+      <button
+        type='button'
+        className='ml-5 px-1 py-0.5 rounded-md bg-[#248045] text-base text-white font-medium'>
+        친구 추가하기
+      </button>
     </CommonHeader>
   )
 }
