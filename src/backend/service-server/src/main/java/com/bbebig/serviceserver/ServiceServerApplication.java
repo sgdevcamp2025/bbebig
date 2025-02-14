0ffc2987b127c642dc99d6354f1d27672b0acbb3
soeun2537 <soeun2537@gmail.com>
2025-02-15 00:48:57 +0900

    fix: 리졸버 안 타는 문제 해결

--- src/backend/service-server/src/main/java/com/bbebig/serviceserver/ServiceServerApplication.java
+++ src/backend/service-server/src/main/java/com/bbebig/serviceserver/ServiceServerApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(basePackages = "com.bbebig.commonmodule")
 @Import(WebMvcConfig.class)
 public class ServiceServerApplication {
 
