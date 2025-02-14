232e30c16cda222d5ee6b68ac26f699a5eb0afd4
seojeongwoo <qw4643987@naver.com>
2025-02-14 15:53:36 +0900

    feat: 네비게이션 타입 추가

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/navigation/DiscordRoute.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/navigation/DiscordRoute.kt
@@ -3,4 +3,19 @@ package com.smilegate.bbebig.presentation.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object DiscordHome
+data object HomeNavGraph
+
+@Serializable
+data object Home
+
+@Serializable
+data object SerVerJoin
+
+@Serializable
+data object Search
+
+@Serializable
+data object LiveChat
+
+@Serializable
+data object CreateServer
