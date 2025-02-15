7031235d4b08576a6be0c99a5bfea4b5e7f432a5
soeun2537 <soeun2537@gmail.com>
2025-02-15 15:29:34 +0900

    feat: passport 연결

--- src/backend/chat-server/src/main/java/com/bbebig/chatserver/ChatServerApplication.java
+++ src/backend/chat-server/src/main/java/com/bbebig/chatserver/ChatServerApplication.java
@@ -1,14 +1,22 @@
 package com.bbebig.chatserver;
 
+import com.bbebig.commonmodule.passport.config.WebMvcConfig;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages = {
+		"com.bbebig.chatserver",
+		"com.bbebig.commonmodule"
+})
+@Import(WebMvcConfig.class)
 public class ChatServerApplication {
 
 	public static void main(String[] args) {
