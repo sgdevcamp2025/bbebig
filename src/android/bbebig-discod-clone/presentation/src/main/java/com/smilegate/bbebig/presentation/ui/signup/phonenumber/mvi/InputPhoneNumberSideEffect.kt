b50ddb0f54f18b799d35704720b05b4e853d223c
seojeongwoo <qw4643987@naver.com>
2025-02-15 23:27:21 +0900

    style: 이름 변경

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/signup/phonenumber/mvi/InputPhoneNumberSideEffect.kt
+++
@@ -1,8 +0,0 @@
-package com.smilegate.bbebig.presentation.ui.signup.phonenumber.mvi
-
-import com.smilegate.bbebig.presentation.base.UiSideEffect
-
-sealed interface InputPhoneNumberSideEffect : UiSideEffect {
-    data object NavigateToAuthCode : InputPhoneNumberSideEffect
-    data object ShowErrorText : InputPhoneNumberSideEffect
-}
