3ab31e45889eb57845d43bb698ba728afad8946f
dh1010a <dh1010a@naver.com>
2025-02-16 15:35:52 +0900

    fix: 임포트 구문 수정

--- src/backend/user-server/src/main/java/com/bbebig/userserver/UserServerApplication.java
+++ src/backend/user-server/src/main/java/com/bbebig/userserver/UserServerApplication.java
@@ -1,14 +1,17 @@
 package com.bbebig.userserver;
 
+import com.bbebig.commonmodule.passport.config.WebMvcConfig;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@Import(WebMvcConfig.class)
 public class UserServerApplication {
 
     public static void main(String[] args) {
