4d2ee77f4a15d208dec5b2daee93f07e3d325aaa
Paek Do Hyun <dh1010a@naver.com>
2025-02-18 00:30:11 +0900

    [BE] 검색 서버 메인으로 병합 (#278)
    * fix: 카프카 로그 레벨 수정
    
    * feat: feign client 수정
    
    * fix: redis 저장 형식 변경

---
+++ src/backend/search-server/src/main/java/com/bbebig/searchserver/global/config/SearchRedisConfig.java
@@ -0,0 +1,43 @@
+package com.bbebig.searchserver.global.config;
+
+import com.bbebig.searchserver.domain.history.domain.ChannelChatMessage;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class SearchRedisConfig {
+
+	@Bean
+	public RedisTemplate<String, ChannelChatMessage> redisChannelChatMessageTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, ChannelChatMessage> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(connectionFactory);
+
+		ObjectMapper objectMapper = createObjectMapper();
+
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+
+		return redisTemplate;
+	}
+
+	private ObjectMapper createObjectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.activateDefaultTyping(
+				objectMapper.getPolymorphicTypeValidator(),
+				ObjectMapper.DefaultTyping.NON_FINAL,
+				JsonTypeInfo.As.PROPERTY
+		);
+		return objectMapper;
+	}
+
+}
