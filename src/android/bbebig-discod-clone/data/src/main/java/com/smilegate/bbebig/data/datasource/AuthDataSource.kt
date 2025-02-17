5188d4e4c4347e9c37e4c35e65042bdefc16c52d
seojeongwoo <qw4643987@naver.com>
2025-02-17 14:47:20 +0900

    feat: 로그인 데이터 소스 구현

--- src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/datasource/AuthDataSource.kt
+++ src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/datasource/AuthDataSource.kt
@@ -1,8 +1,11 @@
 package com.smilegate.bbebig.data.datasource
 
+import com.smilegate.bbebig.data.model.LoginDataModel
 import com.smilegate.bbebig.data.model.SignUpDataModel
+import com.smilegate.bbebig.data.request.LoginRequest
 import com.smilegate.bbebig.data.request.SignUpRequest
 
 interface AuthDataSource {
     suspend fun signUp(request: SignUpRequest): SignUpDataModel
+    suspend fun login(request: LoginRequest): LoginDataModel
 }
