7c7ba2097b39f8cedb9c7bb9ad6c876bcae51e30
dh1010a <dh1010a@naver.com>
2025-02-17 19:26:25 +0900

    fix: 레디스 dto 역직렬화 오류 수정

--- src/backend/common-module/src/main/java/com/bbebig/commonmodule/redis/config/RedisConfig.java
+++ src/backend/common-module/src/main/java/com/bbebig/commonmodule/redis/config/RedisConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -22,7 +23,7 @@ public class RedisConfig {
 		RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(connectionFactory);
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Long.class));
 
 		return redisTemplate;
 	}
