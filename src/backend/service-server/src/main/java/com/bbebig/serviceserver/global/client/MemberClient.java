fa04a8aaa2356ce0812881c19405adf85db06d12
dh1010a <dh1010a@naver.com>
2025-02-17 10:39:39 +0900

    fix: 페인 클라이언트 엔드포인트 변경

--- src/backend/service-server/src/main/java/com/bbebig/serviceserver/global/client/MemberClient.java
+++ src/backend/service-server/src/main/java/com/bbebig/serviceserver/global/client/MemberClient.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "user-server")
 public interface MemberClient {
 
-	@GetMapping("/members/feign/{memberId}")
+	@GetMapping("/feign/members/{memberId}")
 	MemberInfoResponseDto getMemberInfo(@PathVariable(value = "memberId") Long memberId);
 }
