7245edc371c97c420b785bb364dda7f29dc0279c
seojeongwoo <qw4643987@naver.com>
2025-02-17 14:47:09 +0900

    feat: 로그인 api 서비스 구현

--- src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/api/AuthApiService.kt
+++ src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/api/AuthApiService.kt
@@ -1,13 +1,21 @@
 package com.smilegate.bbebig.data.api
 
+import com.smilegate.bbebig.data.base.BaseResponse
+import com.smilegate.bbebig.data.request.LoginRequest
 import com.smilegate.bbebig.data.request.SignUpRequest
+import com.smilegate.bbebig.data.response.LoginResponse
 import com.smilegate.bbebig.data.response.SignUpResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApiService {
-    @POST("auth-server/register")
+    @POST("auth-server/auth/register")
     suspend fun signUp(
         @Body signUpRequest: SignUpRequest,
     ): SignUpResponse
+
+    @POST("auth-server/auth/mobile-login")
+    suspend fun login(
+        @Body loginRequest: LoginRequest,
+    ): BaseResponse<LoginResponse>
 }
