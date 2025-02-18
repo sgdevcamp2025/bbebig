e04773a7e6afd3c324f0235fe02e87bfa2ca3d04
soeun2537 <soeun2537@gmail.com>
2025-02-18 21:09:16 +0900

    chore: 웹소켓 설정

--- src/backend/signaling-server/src/main/java/com/bbebig/signalingserver/global/config/WebSocketConfig.java
+++ src/backend/signaling-server/src/main/java/com/bbebig/signalingserver/global/config/WebSocketConfig.java
@@ -13,11 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stream") // WebSocket 접속 시 endpoint 설정
-                .setAllowedOrigins("*") // TODO: 배포 후 도메인 설정
-                .withSockJS(); // 브라우저에서 WebSocket 을 지원하지 않는 경우에 SockJS 사용
-
-        registry.addEndpoint("/ws-stream")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*"); // TODO: 배포 후 도메인 설정
     }
 
     @Override
