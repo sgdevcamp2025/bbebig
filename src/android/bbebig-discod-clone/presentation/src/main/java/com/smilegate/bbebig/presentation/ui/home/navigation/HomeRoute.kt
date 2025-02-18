ee2720a1327bf1c0f521b8b402e4f8932c3b4b87
seojeongwoo <qw4643987@naver.com>
2025-02-18 19:27:10 +0900

    feat: 홈 뷰모델 생성

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/home/navigation/HomeRoute.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/home/navigation/HomeRoute.kt
@@ -1,10 +1,14 @@
 package com.smilegate.bbebig.presentation.ui.home.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.smilegate.bbebig.presentation.ui.home.HomeScreen
+import com.smilegate.bbebig.presentation.ui.home.HomeViewModel
 
 @Composable
-fun HomeRoute() {
+fun HomeRoute(
+    homeViewModel: HomeViewModel = hiltViewModel(),
+) {
     HomeScreen(
         onMakeServerClick = {},
         onServerJoinClick = {},
