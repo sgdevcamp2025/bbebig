3cce2e1fa18f5df534a13dad1094725b2bfde397
dh1010a <dh1010a@naver.com>
2025-02-20 13:36:46 +0900

    feat: state 클라이언트 주소 변경

--- src/backend/service-server/src/main/java/com/bbebig/serviceserver/global/client/StateClient.java
+++ src/backend/service-server/src/main/java/com/bbebig/serviceserver/global/client/StateClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "service-server")
+@FeignClient(name = "state-server")
 public interface StateClient {
 
 
