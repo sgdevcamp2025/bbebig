9d80dd2a22ca58d49affabbdd2e48c80c9a795fb
seojeongwoo <qw4643987@naver.com>
2025-02-21 17:15:15 +0900

    feat: 서버 생성 데이터 소스 구현

--- src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/datasource/ServerDataSource.kt
+++ src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/datasource/ServerDataSource.kt
@@ -1,9 +1,12 @@
 package com.smilegate.bbebig.data.datasource
 
+import com.smilegate.bbebig.data.model.ServerCreateDataModel
 import com.smilegate.bbebig.data.model.ServerDataModel
 import com.smilegate.bbebig.data.model.ServerInfoDataModel
+import com.smilegate.bbebig.data.request.ServerCreateRequest
 
 interface ServerDataSource {
     suspend fun getServerList(): List<ServerDataModel>
     suspend fun getServerInfo(serverId: Long): ServerInfoDataModel
+    suspend fun createServer(serverCreateRequest: ServerCreateRequest): ServerCreateDataModel
 }
