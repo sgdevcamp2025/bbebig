94bc86a690f590d8347079505eafa6f8188e136b
seojeongwoo <qw4643987@naver.com>
2025-02-21 16:58:57 +0900

    feat: 서버 정보 get 하는 데이터 소스 구현

--- src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/datasource/ServerDataSourceImpl.kt
+++ src/android/bbebig-discod-clone/data/src/main/java/com/smilegate/bbebig/data/datasource/ServerDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.smilegate.bbebig.data.datasource
 import com.smilegate.bbebig.data.api.ServerApiService
 import com.smilegate.bbebig.data.base.callApi
 import com.smilegate.bbebig.data.model.ServerDataModel
+import com.smilegate.bbebig.data.model.ServerInfoDataModel
 import com.smilegate.bbebig.data.model.toDataModel
 import javax.inject.Inject
 
@@ -11,5 +12,9 @@ class ServerDataSourceImpl @Inject constructor(
 ) : ServerDataSource {
     override suspend fun getServerList(): List<ServerDataModel> {
         return callApi { serverService.getServerList() }.servers.toDataModel()
+    }
+
+    override suspend fun getServerInfo(serverId: Long): ServerInfoDataModel {
+        return callApi { serverService.getServerInfo(serverId) }.toDataModel()
     }
 }
