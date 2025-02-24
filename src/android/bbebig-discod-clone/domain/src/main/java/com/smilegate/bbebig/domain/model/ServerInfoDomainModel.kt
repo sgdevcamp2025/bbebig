32b5a0db768d9e012fad2fec828a6fe9941e3301
seojeongwoo <qw4643987@naver.com>
2025-02-24 20:50:40 +0900

    feat: 타입 수정

--- src/android/bbebig-discod-clone/domain/src/main/java/com/smilegate/bbebig/domain/model/ServerInfoDomainModel.kt
+++ src/android/bbebig-discod-clone/domain/src/main/java/com/smilegate/bbebig/domain/model/ServerInfoDomainModel.kt
@@ -3,8 +3,8 @@ package com.smilegate.bbebig.domain.model
 import com.smilegate.bbebig.data.model.ServerInfoDataModel
 
 data class ServerInfoDomainModel(
-    val categoryInfoList: List<CategoryInfoDomainModel>,
-    val channelInfoList: List<ChannelInfoDomainModel>,
+    val categoryMap: Map<Long?, List<CategoryInfoDomainModel>>,
+    val channelList: List<Pair<Long?, List<ChannelInfoDomainModel>>>,
     val ownerId: Long,
     val serverId: Long,
     val serverImageUrl: String,
@@ -12,10 +12,10 @@ data class ServerInfoDomainModel(
 )
 
 fun ServerInfoDataModel.toDomainModel(): ServerInfoDomainModel = ServerInfoDomainModel(
-    categoryInfoList = categoryInfoList.map { it.toDomainModel() },
-    channelInfoList = channelInfoList.map { it.toDomainModel() },
     ownerId = ownerId,
     serverId = serverId,
     serverImageUrl = serverImageUrl,
     serverName = serverName,
+    categoryMap = emptyMap(),
+    channelList = emptyList(),
 )
