73266c911d9e5115e2da460b0e25bd2f4bda493d
seojeongwoo <qw4643987@naver.com>
2025-02-24 20:51:20 +0900

    feat: 오타 수정된 것으로 적용

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/serverjoin/navigation/ServerJoinNavigation.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/serverjoin/navigation/ServerJoinNavigation.kt
@@ -3,19 +3,19 @@ package com.smilegate.bbebig.presentation.ui.serverjoin.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.smilegate.bbebig.presentation.navigation.SerVerJoin
+import com.smilegate.bbebig.presentation.navigation.ServerJoin
 import com.smilegate.bbebig.presentation.ui.serverjoin.ServerJoinScreen
 
 fun NavController.navigateToServerJoin() {
     navigate(
-        route = SerVerJoin,
+        route = ServerJoin,
     )
 }
 
 fun NavGraphBuilder.serverJoinNavigation(
     onBackClick: () -> Unit,
 ) {
-    composable<SerVerJoin> {
+    composable<ServerJoin> {
         ServerJoinScreen(
             onClickBack = {},
             onClickServerJoin = {},
