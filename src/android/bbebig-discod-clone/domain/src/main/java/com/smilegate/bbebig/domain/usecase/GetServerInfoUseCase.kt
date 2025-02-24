17e3c1cce6db5613ae941af28f2d2756347a18b1
seojeongwoo <qw4643987@naver.com>
2025-02-24 20:47:53 +0900

    feat: result로 리턴 타입 수정

--- src/android/bbebig-discod-clone/domain/src/main/java/com/smilegate/bbebig/domain/usecase/GetServerInfoUseCase.kt
+++ src/android/bbebig-discod-clone/domain/src/main/java/com/smilegate/bbebig/domain/usecase/GetServerInfoUseCase.kt
@@ -11,6 +11,22 @@ class GetServerInfoUseCase @Inject constructor(
     private val serverRepository: ServerRepository,
 ) {
     suspend operator fun invoke(serverId: Long): Result<ServerInfoDomainModel> {
-        return runCatching { serverRepository.getServerInfo(serverId).toDomainModel() }
+        return runCatching {
+            val serverInfo = serverRepository.getServerInfo(serverId)
+            val domainModel = serverInfo.toDomainModel()
+            val categoryDomainList = serverInfo.categoryInfoList.toDomainModel()
+            val channelDomainList = serverInfo.channelInfoList.toDomainModel()
+
+            if (categoryDomainList.isEmpty()) {
+                domainModel.copy(
+                    channelList = channelDomainList.groupBy { it.position.toLong() }.toList(),
+                )
+            } else {
+                domainModel.copy(
+                    categoryMap = categoryDomainList.groupBy { it.categoryId },
+                    channelList = channelDomainList.groupBy { it.categoryId }.toList().sortedBy { it.first },
+                )
+            }
+        }
     }
 }
