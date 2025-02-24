13c7517d1ce5bb485ba2066b7c0848683f60ac45
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-02-24 15:05:16 +0900

    chore: 서버 구독 응답 핸들러 정의

---
+++ src/frontend/src/utils/serverEventHandler.ts
@@ -0,0 +1,36 @@
+import { useChatStore } from '@/hooks/store/use-chat-store'
+import { ServerSubscribeResponse } from '@/types/ChatStompEvent'
+
+export const handleServerEvent = (message: ServerSubscribeResponse) => {
+  const { addMessage } = useChatStore.getState()
+
+  switch (message.type) {
+    case 'SERVER_CHANNEL':
+      console.log('SERVER_CHANNEL', message)
+      break
+    case 'SERVER_CATEGORY':
+      console.log('SERVER_CATEGORY', message)
+      break
+    case 'SERVER_ACTION':
+      console.log('SERVER_ACTION', message)
+      break
+    case 'SERVER_MEMBER_ACTION':
+      console.log('SERVER_MEMBER_ACTION', message)
+      break
+    case 'SERVER_MEMBER_PRESENCE':
+      console.log('SERVER_MEMBER_PRESENCE', message)
+      break
+    case 'MESSAGE_CREATE':
+      console.log('MESSAGE_CREATE', message)
+      if (!message.sendMemberId) return
+      console.log('이벤트 핸들러에서 추가!!!')
+      addMessage(message.channelId, {
+        ...message,
+        sendMemberId: message.sendMemberId
+      })
+      break
+    default:
+      console.log(message)
+      break
+  }
+}
