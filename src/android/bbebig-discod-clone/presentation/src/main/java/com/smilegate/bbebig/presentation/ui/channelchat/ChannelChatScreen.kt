b0cac81f23d562b2597463f54ad4a6f764dae6db
seojeongwoo <qw4643987@naver.com>
2025-02-25 19:29:59 +0900

    feat: status padding 추가

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/channelchat/ChannelChatScreen.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/channelchat/ChannelChatScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -45,10 +46,10 @@ fun ChannelChatScreen(
 ) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
 
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
+    Column(modifier = modifier.fillMaxSize().statusBarsPadding(), verticalArrangement = Arrangement.SpaceBetween) {
         DiscordTopBar(
             modifier = Modifier.fillMaxWidth(),
-            title = "Channel Chat",
+            title = channelName,
             backButtonColor = White,
             onBackClick = onClickBack,
             onClickSearch = onClickSearch,
