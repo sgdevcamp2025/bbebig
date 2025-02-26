d1656d524aa875ac4ca56021a13ac228dd6b7248
dh1010a <dh1010a@naver.com>
2025-02-26 17:51:57 +0900

    fix: 모바일용 엔드포인트 수정

--- src/backend/chat-server/src/main/java/com/bbebig/chatserver/global/config/WebSocketConfig.java
+++ src/backend/chat-server/src/main/java/com/bbebig/chatserver/global/config/WebSocketConfig.java
@@ -38,11 +38,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 
 		registry.addEndpoint("/ws-mobile") // 클라이언트가 연결할 엔드포인트 설정
-				.setAllowedOrigins("*");
+				.setAllowedOriginPatterns("*");
 
 		// 웹소켓을 사용할 수 없는 환경에서 sockJS 지원
 		registry.addEndpoint("/ws-chat") // 클라이언트가 연결할 엔드포인트 설정
-				.setAllowedOrigins("*")
+				.setAllowedOriginPatterns("*")
 				.withSockJS();
 
 		registry.setErrorHandler(stompErrorHandler);
