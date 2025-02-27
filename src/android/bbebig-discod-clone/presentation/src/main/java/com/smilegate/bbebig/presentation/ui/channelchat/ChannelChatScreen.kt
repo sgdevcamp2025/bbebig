bc2f9e27c05ee5aa37ec79b3ff8f27102e1da7c2
seojeongwoo <qw4643987@naver.com>
2025-02-27 19:38:39 +0900

    feat: 채팅 리스트 데이터 연동

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/channelchat/ChannelChatScreen.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/channelchat/ChannelChatScreen.kt
@@ -2,20 +2,26 @@ package com.smilegate.bbebig.presentation.ui.channelchat
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,11 +32,17 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.smilegate.bbebig.presentation.component.DiscordLoadingCircular
 import com.smilegate.bbebig.presentation.component.DiscordTextField
 import com.smilegate.bbebig.presentation.component.DiscordTopBar
 import com.smilegate.bbebig.presentation.component.DiscordUserChatBox
+import com.smilegate.bbebig.presentation.theme.Gray15
 import com.smilegate.bbebig.presentation.theme.Gray50
 import com.smilegate.bbebig.presentation.theme.White
+import com.smilegate.bbebig.presentation.ui.home.model.MessageContent
+import com.smilegate.bbebig.presentation.utils.ImmutableList
+import com.smilegate.bbebig.presentation.utils.LocalDateUtil
 import com.smilegate.bbebig.presentation.utils.StableImage
 import com.smilegate.bbebig.presentation.utils.rippleClick
 import com.smilegate.devcamp.presentation.R
@@ -42,26 +54,41 @@ fun ChannelChatScreen(
     onClickSearch: () -> Unit,
     onClickAddPhoto: () -> Unit,
     onClickSendChat: (String) -> Unit,
+    isLoading: Boolean,
     modifier: Modifier = Modifier,
+    chatList: ImmutableList<MessageContent>,
 ) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-
-    Column(modifier = modifier.fillMaxSize().statusBarsPadding(), verticalArrangement = Arrangement.SpaceBetween) {
-        DiscordTopBar(
-            modifier = Modifier.fillMaxWidth(),
-            title = channelName,
-            backButtonColor = White,
-            onBackClick = onClickBack,
-            onClickSearch = onClickSearch,
-        )
-        ChatContentContainer(
-            modifier = Modifier
-                .heightIn(max = screenHeight - 180.dp)
-                .fillMaxSize(),
-            list = listOf( /* TODO: Chat Data */),
-        )
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .statusBarsPadding(),
+    ) {
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            DiscordTopBar(
+                modifier = Modifier.fillMaxWidth(),
+                title = channelName,
+                backButtonColor = White,
+                onBackClick = onClickBack,
+                onClickSearch = onClickSearch,
+            )
+            ChatContentContainer(
+                modifier = Modifier
+                    .heightIn(max = screenHeight - 130.dp)
+                    .fillMaxSize(),
+                messageList = chatList,
+                isLoading = isLoading,
+            )
+        }
         ChatInputContainer(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .background(Gray15)
+                .imePadding()
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
             onClickAddPhoto = onClickAddPhoto,
             onClickSendChat = onClickSendChat,
         )
@@ -69,19 +96,55 @@ fun ChannelChatScreen(
 }
 
 @Composable
-private fun ChatContentContainer(modifier: Modifier, list: List<Any>) {
-    LazyColumn(modifier = modifier) {
-        itemsIndexed(list) { index, content ->
+private fun ChatContentContainer(
+    modifier: Modifier,
+    messageList: ImmutableList<MessageContent>,
+    isLoading: Boolean,
+) {
+    val listState = rememberLazyListState()
+    if (messageList.isNotEmpty()) {
+        LaunchedEffect(messageList) {
+            listState.scrollToItem(messageList.lastIndex)
+        }
+    }
+
+    if (isLoading) {
+        DiscordLoadingCircular(
+            modifier = modifier.fillMaxSize(),
+            backgroundColor = Color.Transparent,
+        )
+    }
+
+    if (messageList.isEmpty()) {
+        EmptyChatContainer(modifier = modifier.fillMaxSize())
+    }
+    LazyColumn(modifier = modifier, state = listState) {
+        itemsIndexed(
+            items = messageList,
+            key = { _, item -> item.id },
+        ) { index, content ->
             DiscordUserChatBox(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 10.dp, vertical = 10.dp),
-                userName = "User Name",
-                date = "2021-09-01",
-                chatContent = "Chat Content",
-                userIconResId = R.drawable.ic_plus,
+                userName = content.userInfo.name,
+                date = LocalDateUtil.chatTime(content.createdAt),
+                chatContent = content.content,
+                userIconUrl = content.userInfo.avatarUrl,
+                colorNumber = content.userInfo.colorNumber,
             )
         }
+    }
+}
+
+@Composable
+private fun EmptyChatContainer(modifier: Modifier) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Text(
+            color = White,
+            fontSize = 20.sp,
+            text = stringResource(R.string.empty_chat),
+        )
     }
 }
 
@@ -126,6 +189,12 @@ private fun ChatInputContainer(
             textFieldState = chatTextState,
             radius = 30.dp,
             textHint = stringResource(R.string.msg_send_hint),
+            onClickSendChat = {
+                if (chatTextState.text.isNotEmpty()) {
+                    onClickSendChat(chatTextState.text.toString())
+                    chatTextState.clearText()
+                }
+            },
         )
         StableImage(
             modifier = Modifier
@@ -134,6 +203,7 @@ private fun ChatInputContainer(
                 .rippleClick {
                     if (chatTextState.text.isNotEmpty()) {
                         onClickSendChat(chatTextState.text.toString())
+                        chatTextState.clearText()
                     }
                 }
                 .size(30.dp)
@@ -152,5 +222,7 @@ private fun ChannelChatScreenPreview() {
         onClickSearch = {},
         onClickAddPhoto = {},
         onClickSendChat = {},
+        chatList = ImmutableList(emptyList()),
+        isLoading = false,
     )
 }
