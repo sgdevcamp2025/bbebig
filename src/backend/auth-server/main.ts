fb364dcabdcab28af1ad9f0c3ed3fc7a5a63bc68
Zero-1016 <iolpd42@gmail.com>
2025-03-03 16:08:43 +0900

    feat: cors 관련 설정을 제거합니다. 게이트웨이만 이용합니다

--- src/backend/auth-server/main.ts
+++ src/backend/auth-server/main.ts
@@ -138,12 +138,8 @@ app.register(currentAuthPlugin);
 app.register(routes);
 
 app.register(cors, {
-  origin: ['http://localhost:5173', 'https://bbebig.netlify.app'],
+  origin: false,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-  exposedHeaders: ['Set-Cookie'],
-  preflightContinue: true,
 });
 
 app
