1fdcce228fde60eb31e6f4cc2eb809b57e0e8044
Zero-1016 <iolpd42@gmail.com>
2025-03-03 15:51:59 +0900

    feat: 모든 domain내 호출 허용

--- src/backend/auth-server/main.ts
+++ src/backend/auth-server/main.ts
@@ -138,11 +138,10 @@ app.register(currentAuthPlugin);
 app.register(routes);
 
 app.register(cors, {
-  origin: ['http://localhost:5173', 'https://bbebig.netlify.app'],
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['set-cookie'],
 });
 
 app
