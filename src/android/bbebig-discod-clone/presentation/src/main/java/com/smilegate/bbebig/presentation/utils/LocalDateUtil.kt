fbfb76eb2cb1150ba5244d4eb454eb1c7a20357e
seojeongwoo <qw4643987@naver.com>
2025-03-05 23:08:54 +0900

    feat: date 관련 함수 추가

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/utils/LocalDateUtil.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/utils/LocalDateUtil.kt
@@ -2,26 +2,29 @@ package com.smilegate.bbebig.presentation.utils
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeFormatterBuilder
-import java.time.format.SignStyle
-import java.time.temporal.ChronoField
+import java.util.Calendar
+import java.util.Locale
 
 object LocalDateUtil {
-    fun format(date: String): String {
-        val inputFormatter = DateTimeFormatterBuilder()
-            .appendPattern("yyyy.")
-            .appendValue(ChronoField.MONTH_OF_YEAR, 1, 2, SignStyle.NOT_NEGATIVE)
-            .appendLiteral('.')
-            .appendValue(ChronoField.DAY_OF_MONTH, 1, 2, SignStyle.NOT_NEGATIVE)
-            .toFormatter()
-
-        return LocalDate.parse(date, inputFormatter).format(DateTimeFormatter.ISO_DATE)
-    }
-
     fun chatTime(date: String): String {
         date.split("T")
         val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val outputFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
         return LocalDate.parse(date.split("T")[0], inputFormatter).format(outputFormatter)
+    }
+
+    fun getCurrentDate(): String {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault())
+        return LocalDate.now().format(formatter)
+    }
+
+    fun getCalenderDate(calendar: Calendar): String {
+        return String.format(
+            Locale.getDefault(),
+            "%04d-%02d-%02d",
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.DAY_OF_MONTH),
+        )
     }
 }
