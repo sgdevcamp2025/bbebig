d1c8bf41fab3d747a95e105a874565992fb0c0ab
seojeongwoo <qw4643987@naver.com>
2025-03-05 23:07:06 +0900

    feat: popUp할 아이디 값 수정

--- src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/home/navigation/HomeNavigation.kt
+++ src/android/bbebig-discod-clone/presentation/src/main/java/com/smilegate/bbebig/presentation/ui/home/navigation/HomeNavigation.kt
@@ -1,14 +1,13 @@
 package com.smilegate.bbebig.presentation.ui.home.navigation
 
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.smilegate.bbebig.presentation.navigation.Home
 
 fun NavController.navigateToHome() {
     navigate(Home) {
-        popUpTo(graph.findStartDestination().id) {
+        popUpTo(graph.id) {
             inclusive = true
         }
         launchSingleTop = true
