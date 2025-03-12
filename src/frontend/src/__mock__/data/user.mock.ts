e785d60be785f7d7966eb9a2cbe9a819cf0e5398
Zero-1016 <iolpd42@gmail.com>
2025-03-12 23:09:37 +0900

    test: user mock 데이터를 추가한다.

---
+++ src/frontend/src/__mock__/data/user.mock.ts
@@ -0,0 +1,53 @@
+import { User } from '@sentry/react'
+
+export const userMock = {
+  id: 1,
+  name: 'test',
+  nickname: 'test',
+  email: 'test@test.com',
+  birthdate: '2021-01-01',
+  avatarUrl: null,
+  bannerUrl: null,
+  introduce: null,
+  presenceStatus: 'ONLINE',
+  lastAccessAt: '2021-01-01T00:00:00.000Z'
+} as User
+
+export const friendsMock = [
+  {
+    friendId: 0,
+    memberId: 0,
+    memberName: 'test',
+    memberNickname: 'test',
+    memberAvatarUrl: null,
+    memberBannerUrl: null,
+    memberIntroduce: null,
+    memberEmail: 'test@test1.com',
+    globalStatus: 'ONLINE',
+    createdAt: '2025-03-12T13:58:09.724Z'
+  },
+  {
+    friendId: 1,
+    memberId: 2,
+    memberName: 'test',
+    memberNickname: 'test',
+    memberAvatarUrl: null,
+    memberBannerUrl: null,
+    memberIntroduce: null,
+    memberEmail: 'test@test2.com',
+    globalStatus: '',
+    createdAt: '2025-03-12T13:58:09.724Z'
+  },
+  {
+    friendId: 2,
+    memberId: 3,
+    memberName: 'test',
+    memberNickname: 'test',
+    memberAvatarUrl: null,
+    memberBannerUrl: null,
+    memberIntroduce: null,
+    memberEmail: 'test@test3.com',
+    globalStatus: 'ONLINE',
+    createdAt: '2025-03-12T13:58:09.724Z'
+  }
+]
--- src/frontend/src/__mock__/worker.ts
+++ src/frontend/src/__mock__/worker.ts
@@ -2,5 +2,6 @@ import { setupWorker } from 'msw/browser'
 
 import { authHandler } from './handlers/auth.handler'
 import { serviceHandler } from './handlers/service.handler'
+import { userHandler } from './handlers/user.handler'
 
-export const worker = setupWorker(...authHandler, ...serviceHandler)
+export const worker = setupWorker(...authHandler, ...serviceHandler, ...userHandler)
